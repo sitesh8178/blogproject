53c5408e886d0a6ff7e74dd5d1e94a7e47b16667
sitesh8178 <102595914+sitesh8178@users.noreply.github.com>
2022-03-29 17:47:32 +0530

    Add files via upload

---
+++ blog/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from blog.models import Comments
+
+class EmailSendForm(forms.Form):
+    name =forms.CharField()
+    email=forms.EmailField()
+    to=forms.EmailField()
+    comments=forms.CharField(widget=forms.Textarea, required=False)
+
+class CommentsForm(forms.ModelForm):
+    class Meta:
+        model=Comments
+        fields = ('name','email','body')
